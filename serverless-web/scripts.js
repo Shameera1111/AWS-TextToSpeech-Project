77131a51346e61e056b8f3b9fa9180147c263995
Shameera Shaik <shaikshameera468@gmail.com>
2025-07-04 02:41:20 +0530

    updated serverless-web files including error.html

--- serverless-web/scripts.js
+++ serverless-web/scripts.js
@@ -1,62 +1,57 @@
-var API_ENDPOINT = "API endpoint"
+var API_ENDPOINT = "API endpoint";
 
-document.getElementById("sayButton").onclick = function(){
+$(document).ready(function() {
+    $("#sayButton").click(function() {
+        var inputData = {
+            "voice": $('#voiceSelected option:selected').val(),
+            "text": $('#postText').val()
+        };
 
-	var inputData = {
-		"voice": $('#voiceSelected option:selected').val(),
-		"text" : $('#postText').val()
-	};
+        $.ajax({
+            url: API_ENDPOINT,
+            type: 'POST',
+            data: JSON.stringify(inputData),
+            contentType: 'application/json; charset=utf-8',
+            success: function(response) {
+                $("#postIDreturned").text("Message : " + response);
+            },
+            error: function() {
+                alert("error");
+            }
+        });
+    });
 
-	$.ajax({
-	      url: API_ENDPOINT,
-	      type: 'POST',
-	      data:  JSON.stringify(inputData)  ,
-	      contentType: 'application/json; charset=utf-8',
-	      success: function (response) {
-					document.getElementById("postIDreturned").textContent="Message : " + response;
-	      },
-	      error: function () {
-	          alert("error");
-	      }
-	  });
-}
+    $("#searchButton").click(function() {
+        $.ajax({
+            url: API_ENDPOINT,
+            type: 'GET',
+            success: function(response) {
+                $('#posts tbody tr').remove();
 
+                $.each(response, function(i, data) {
+                    var player = "";
+                    if (typeof data['url'] !== "undefined") {
+                        player = "<audio controls><source src='" + data['url'] + "' type='audio/mpeg'></audio>";
+                    }
 
-document.getElementById("searchButton").onclick = function(){
+                    $("#posts tbody").append(
+                        "<tr>" +
+                        "<td data-label='Voice'>" + data['selected voice'] + "</td>" +
+                        "<td data-label='Post'>" + data['input text'] + "</td>" +
+                        "<td data-label='Status'>" + data['status'] + "</td>" +
+                        "<td data-label='Player'>" + player + "</td>" +
+                        "</tr>"
+                    );
+                });
+            },
+            error: function() {
+                alert("error");
+            }
+        });
+    });
 
-	var postId = $('#postId').val();
-
-
-	$.ajax({
-				url: API_ENDPOINT,
-				type: 'GET',
-				success: function (response) {
-
-					$('#posts tr').slice(1).remove();
-
-	        jQuery.each(response, function(i,data) {
-
-						var player = "<audio controls><source src='" + data['url'] + "' type='audio/mpeg'></audio>"
-
-						if (typeof data['url'] === "undefined") {
-	    				var player = ""
-						}
-
-						$("#posts").append("<tr>\
-								<td>" + data['selected voice'] + "</td> \
-								<td>" + data['input text'] + "</td> \
-								<td>" + data['status'] + "</td> \
-								<td>" + player + "</td> \
-								</tr>");
-	        });
-				},
-				error: function () {
-						alert("error");
-				}
-		});
-}
-
-document.getElementById("postText").onkeyup = function(){
-	var length = $(postText).val().length;
-	document.getElementById("charCounter").textContent="Characters: " + length;
-}
+    $("#postText").on("keyup", function() {
+        var length = $(this).val().length;
+        $("#charCounter").text("Characters: " + length);
+    });
+});
